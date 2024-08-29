a19ec66ff9b62ecc59b5f2fe1d396ebfe45342ad
Hishen Sandaru Perera <165247062+HishenS@users.noreply.github.com>
2024-08-29 12:57:13 +0530

    Add files via upload

---
+++ AboutUsPage/AboutUsPage.php
@@ -0,0 +1,95 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang = "en" dir = "ltr">
+	<head>
+		<meta charset = "utf-8">
+		<meta name = "viewpoint" content = "width=device-width, initial-scale=1.0">
+		<title>About Us</title>
+		<link rel = "stylesheet" href = "./about.css">
+	</head>
+
+    <body>
+        <header>
+			
+		</header>
+
+		
+        <div class = "topic">
+		</br></br></br></br></br>
+            <h1>About AdHandiya</h1>
+            <h3>Welcome to AdHandiya, where strategy and creativity join together to improve the online visibility of your company.
+				 Being a top online advertising company, we are experts at creating customized digital campaigns that pique interest, encourage interaction, 
+				 and eventually increase your revenue. Utilizing state-of-the-art technology and data-driven insights, 
+				 our team of enthusiastic experts strives to stay ahead of market trends and produces outcomes that beyond expectations. 
+				 We can help turn your idea become a reality whether you're a startup trying to build your internet presence or an established brand trying to reach a wider audience.
+				 Come along on our road to digital success, where every impression matters and every click counts.
+			</h3>
+        </div>
+		
+
+		<div class = "container">
+		<section class = "about">
+			<div class = "about-pic">
+				<img src = "./images/F1.webp">
+			</div>
+			<div class = "about-cnt">
+				<h2><u>Our Services</u></h2>
+				<p>AdHandiya offers you with comprehensive solutions including campaign management, creative design, audience targeting, and analytics.</p>
+			</br><ul>
+					<li><h4>Efficient service</h4>
+						<p>AdHandiya, an online advertising agency, offers efficient services tailored to meet diverse advertising needs. From expert campaign management to creative design, 
+							audience targeting, and analytics, we deliver optimal solutions for maximum impact and results, ensuring client satisfaction and success in the digital landscape.</p>
+					</li></br>
+					<li><h4>Content Marketing</h4>
+						<p>Build brand authority and engage your audience with compelling content. Our content marketing services include strategy development, content creation, distribution, and performance tracking to drive meaningful results.</p>
+					</li></br>
+					<li><h4>PPC Advertising</h4>
+						<p>Maximize your ROI with our pay-per-click advertising solutions. We develop targeted campaigns, optimize ad performance, and track results to ensure cost-effective and impactful advertising campaigns.</p>
+					</li>
+				</ul>
+				
+			</div>
+		</section>
+
+		<section class = "about">
+				<div class = "about-cnt">
+				<h2><u>How we work</u></h2></br>
+					<h4>How does our service work?</h4>
+					<h5>Our service works by first understanding your goals and target audience. Then, we develop tailored strategies encompassing various digital marketing channels such as SEO, social media, PPC, email, and content marketing. Through continuous optimization and analysis, we ensure optimal performance and results.</h5>
+				</br>
+					<h4>Who do we work with?</h4>
+					<h5>We collaborate with businesses of all sizes, from startups to established enterprises, across diverse industries. Our clients value our expertise in digital marketing and trust us to deliver effective solutions tailored to their unique needs and goals.</h5>
+				
+			</div>
+		</section>
+
+		<section class = "about">
+			<div class = "about-cnt">
+			<h2><u>Payments</u></h2></br>
+				<h4>Allows all kinds of payment types</h4>
+				<h5>Our payment solutions streamline transaction processes, offering secure online payment gateways, versatile billing options, and efficient invoicing systems. We prioritize user convenience and data security, ensuring smooth financial transactions for businesses and seamless shopping experiences for customers, fostering trust and satisfaction.</h5>
+			</div>
+			</section> 
+			
+		</div>
+	</section>
+		</div>
+
+	<section class = "bottom">
+		<div class = "bottom-cnt">
+			<h3>More about AdHandiya</h3></br>
+			<a href = "#"><u>Terms and conditions</u></a></br>
+			<a href = "#"><u>Policy statement</u></a>
+		</div>
+		<div class = "bottom-help">
+			<h3>Ask anything about us</h3>
+			<a href = "FAQ/index.html"><u>FAQs</u></a>
+			<a href = "#"><u>ContactUs</u></a>
+		</div>
+	</section>
+		
+
+    </body>
+
+</html>
